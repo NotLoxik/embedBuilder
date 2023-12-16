5295d8e6c82b6fcf2894406fe038707c13ae8f1a
Loxik <hello@loxik.dev>
2023-12-16 19:47:22 +0100

    🔨 Update builder.config.js

--- builder.config.js
+++ builder.config.js
@@ -1,22 +1,22 @@
 options = {
-    username: 'Discord Bot',
-    avatar: 'https://cdn.discordapp.com/embed/avatars/3.png',
-    verified: false,
-    noUser: false,
-    data: null,
-    guiTabs: ['author', 'description'],
-    useJsonEditor: false,
-    reverseColumns: false,
-    allowPlaceholders: false,
-    autoUpdateURL: true, // Automatically update data from the JSON file
-    autoParams: true, // Automatically update data from options menu ex. multi-emebeds
-    hideEditor: false,
-    hidePreview: false,
-    hideMenu: false,
-    single: false,
-    noMultiEmbedsOption: true, // Allow users to have multiple embeds in one message
-    sourceOption: false, // Display link to source code in menu.
-}
+  username: "Discord Bot",
+  avatar: "https://cdn.discordapp.com/embed/avatars/3.png",
+  verified: false,
+  noUser: false,
+  data: null,
+  guiTabs: ["author", "description"],
+  useJsonEditor: false,
+  reverseColumns: false,
+  allowPlaceholders: false,
+  autoUpdateURL: true, // Automatically update data from the JSON file
+  autoParams: true, // Automatically update data from options menu ex. multi-emebeds
+  hideEditor: false,
+  hidePreview: false,
+  hideMenu: false,
+  single: false,
+  noMultiEmbedsOption: true, // Allow users to have multiple embeds in one message
+  sourceOption: false, // Display link to source code in menu.
+};
 
 /**
  * Code that will run after page had loaded
